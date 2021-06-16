1984cfeddb7f1b848f891cd01bc6884beeff6ec4
topdeveloper0729 <topdev0729@gmail.com>
2021-06-16 21:06:59 +0900

    fix: mobile drag and drop

--- index.js
+++ index.js
@@ -1,17 +1,82 @@
 $(document).ready(function() {
-    $(".player__item").draggable({
-        containment: ".field__box",
-        cursor: "grabbing"
-    })
+    for (let index = 1; index < 12; index++) {
+        var dragItem = document.querySelector(`#player${index}`);
+        var container = document.querySelector("#playground");
+
+        var active = false;
+        var currentX;
+        var currentY;
+        var initialX;
+        var initialY;
+        var xOffset = 0;
+        var yOffset = 0;
+
+        container.addEventListener("touchstart", dragStart, false);
+        container.addEventListener("touchend", dragEnd, false);
+        container.addEventListener("touchmove", drag, false);
+
+        container.addEventListener("mousedown", dragStart, false);
+        container.addEventListener("mouseup", dragEnd, false);
+        container.addEventListener("mousemove", drag, false);
+
+        function dragStart(e) {
+            console.log("start");
+            if (e.type === "touchstart") {
+                initialX = e.touches[0].clientX - xOffset;
+                initialY = e.touches[0].clientY - yOffset;
+            } else {
+                initialX = e.clientX - xOffset;
+                initialY = e.clientY - yOffset;
+            }
+
+            if (e.target === dragItem) {
+                active = true;
+            }
+        }
+
+        function dragEnd(e) {
+            console.log("end");
+            initialX = currentX;
+            initialY = currentY;
+
+            active = false;
+        }
+
+        function drag(e) {
+            console.log("drag");
+            if (active) {
+
+                e.preventDefault();
+
+                if (e.type === "touchmove") {
+                    currentX = e.touches[0].clientX - initialX;
+                    currentY = e.touches[0].clientY - initialY;
+                } else {
+                    currentX = e.clientX - initialX;
+                    currentY = e.clientY - initialY;
+                }
+
+                xOffset = currentX;
+                yOffset = currentY;
+
+                setTranslate(currentX, currentY, dragItem);
+            }
+        }
+
+        function setTranslate(xPos, yPos, el) {
+            console.log("setTranslatesetTranslate");
+            el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+        }
+    }
 });
 
 //When mouseover the player, show card and change box-shadow color
-$(".player__item").on('mouseover', function() {
-    $(this).find(".player__item--card").css("display", "flex");
-    //$(this).(".player__item").css("box-shadow", "0 0 30px blue");
-});
+// $(".player__item").on("mouseover", function() {
+//     $(this).find(".player__item--card").css("display", "flex");
+//     //$(this).(".player__item").css("box-shadow", "0 0 30px blue");
+// });
 
-//When mouseout the player, hidden card and change box-shadow color
-$(".player__item").on('mouseout', function() {
-    $(this).find(".player__item--card").css("display", "none");
-});
+// //When mouseout the player, hidden card and change box-shadow color
+// $(".player__item").on("mouseout", function() {
+//     $(this).find(".player__item--card").css("display", "none");
+// });
