a6a12e5f3ff559d83049bb98a50fc0339bf77fc1
topdeveloper0729 <topdev0729@gmail.com>
2021-06-16 20:21:38 +0900

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $(".player__item").draggable({
+   containment:".field__box",
+   cursor: "grabbing"
+  })
+});
+
+//When mouseover the player, show card and change box-shadow color
+$(".player__item").on('mouseover', function(){
+  $(this).find(".player__item--card").css("display", "flex");
+  //$(this).(".player__item").css("box-shadow", "0 0 30px blue");
+});
+
+//When mouseout the player, hidden card and change box-shadow color
+$(".player__item").on('mouseout', function(){
+  $(this).find(".player__item--card").css("display", "none");
+});
